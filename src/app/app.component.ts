37e15cf3b2ec14acb4520d0df925c0eaf9fcacf2
sergey <serg.nefedoff@yandex.ru>
2021-03-22 13:52:02 +0300

    Dropdown: base implementation (add click event to button)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +6,9 @@ import { DropdownComponent } from './components/dropdown/dropdown.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // open = false;
+  public open = false;
 
-  // @ViewChild(DropdownComponent)
-  // component?: DropdownComponent;
-
-  // onClick() {
-  //   this.open = false;
-  //
-  //   if (this.component && this.component.nativeFocusableElement) {
-  //     this.component.nativeFocusableElement.focus();
-  //   }
-  // }
+  public onClick(): void {
+    this.open = !this.open;
+  }
 }
--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,31 +1,11 @@
 import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { DropdownDirective } from '../../directives/dropdown.directive';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss']
 })
-export class DropdownComponent implements OnInit {
+export class DropdownComponent {
   @Input() public content!: TemplateRef<any>;
-
-  open = true;
-
-  @ViewChild(DropdownDirective)
-  private dropdownDirective?: DropdownDirective;
-
-  get dropdown(): HTMLElement | null {
-    return !this.dropdownDirective || this.dropdownDirective.dropdownBoxRef === null
-      ? null
-      : this.dropdownDirective.dropdownBoxRef.location.nativeElement;
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.open = false;
-    }, 2000);
-  }
-
+  @Input('isVisible') open = false;
 }
